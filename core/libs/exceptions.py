b7fd1ac64223e4cd38cda78da5d183632af9bfdd
sheersh1111 <106367218+sheersh1111@users.noreply.github.com>
2023-04-05 09:15:20 -0400

    Update exceptions.py

--- core/libs/exceptions.py
+++ core/libs/exceptions.py
@@ -11,15 +11,15 @@ class FyleError(Exception):
         res['message'] = self.message
         return res
     
-class ValidationError(Exception):
-    status_code = 400
+# class ValidationError(Exception):
+#     status_code = 400
 
-    def __init__(self, status_code, message):
-        Exception.__init__(self)
-        self.message = message
-        self.status_code = status_code
+#     def __init__(self, status_code, message):
+#         Exception.__init__(self)
+#         self.message = message
+#         self.status_code = status_code
 
-    def to_dict(self):
-        res = dict()
-        res['message'] = self.message
-        return res
+#     def to_dict(self):
+#         res = dict()
+#         res['message'] = self.message
+#         return res
